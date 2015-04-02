6dec1a2729db9e070aa99807b14733715a5d4849
Jamie Yung <jamie.yung286@gmail.com>
2015-04-03 00:19:56 +1100

    Added fullstack scaffold generated by angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('0010FullstackMongoApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
