6311e8b6b635422c79cfd0755ae6c1302515a456
Jamie Yung <jamie.yung286@gmail.com>
2015-04-05 10:44:23 +1000

    Added a 'groups' route and a 'groups' API endpoint, successfully synced PUT and GET with the database!

---
+++ client/app/groups/groups.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('0010FullstackMongoApp')
+  .controller('GroupsCtrl', function ($scope, $http, socket) {
+    $scope.message = 'Hello';
+
+    $http.get('api/groups').success(function(groups) {
+    	$scope.groups = groups;
+    	socket.syncUpdates('group', $scope.groups);
+    });
+
+    $scope.$on('$destroy', function(){
+    	socket.unsyncUpdates('group');
+    });
+
+    $scope.post = function() {
+    	$http.post('api/groups', {name: 'Hello'});
+    };
+  });
