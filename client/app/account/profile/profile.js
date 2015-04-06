846ccf67c25f297d238c79a8770c66658921a573
Paul Glavidis <duelingnetworkforums@gmail.com>
2015-04-07 08:53:41 +1000

    Added profile route
    Profile route appears before "admin" but after "home" and only when
    logged in. Redirects and logged out users.

---
+++ client/app/account/profile/profile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('theatreProjApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'app/account/profile/profile.html',
+        controller: 'ProfileCtrl',
+        authenticate: true
+      });
+  });
