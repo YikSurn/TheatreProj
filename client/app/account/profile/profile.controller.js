2338361aae6c776a30b75af3286d8ae46c9376d8
Paul Glavidis <duelingnetworkforums@gmail.com>
2015-04-08 09:41:21 +1000

    Added slight functionality to Profile route.

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -2,5 +2,25 @@
 
 angular.module('theatreProjApp')
   .controller('ProfileCtrl', function ($scope, Auth) {
-    $scope.getCurrentUser = Auth.getCurrentUser;
-  });   
+  $scope.name = Auth.getCurrentUser().name;
+  $scope.email = Auth.getCurrentUser().email;
+  $scope.role = Auth.getCurrentUser().role;
+  var user = Auth.getCurrentUser();
+  $scope.user = user;
+  $scope.editorEnabled = false;
+  
+  $scope.enableEditor = function() {
+    $scope.editorEnabled = true;
+    $scope.editableEmail = $scope.email;
+  };
+  
+  $scope.disableEditor = function() {
+    $scope.editorEnabled = false;
+  };
+  
+  $scope.save = function() {
+    $scope.email = $scope.editableEmail;
+    user["email"] = $scope.email;
+    $scope.disableEditor();
+  };
+});   
