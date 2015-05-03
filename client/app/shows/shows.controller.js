f305c2f1733bb18f4c6cea8831d41ef8f034739e
Paul Glavidis <duelingnetworkforums@gmail.com>
2015-05-03 15:24:53 +1000

    Added Modal Dialogs to projects with some CRUD

--- client/app/shows/shows.controller.js
+++ client/app/shows/shows.controller.js
@@ -14,7 +14,6 @@ angular.module('theatreProjApp')
         socket.syncUpdates('group', $scope.groups)
     });
 
-
     $scope.$on('$destroy', function(){
     	socket.unsyncUpdates('projectshow');
     });
@@ -32,7 +31,7 @@ angular.module('theatreProjApp')
     };
     $scope.toggleMin();
 
-    /*Function to create a new show*/
+    /*Function to create a new project*/
     $scope.createProject = function(showName, showGroup, showStatus, dt) {
         $scope.showName = showName;
         $scope.showGroup = showGroup;
@@ -44,4 +43,47 @@ angular.module('theatreProjApp')
         $scope.createIsCollapsed = true;
     };
 
+    /*Opens Modal Dialog with new controller*/
+    $scope.open = function (project) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'projectViewModal.html',
+            controller: 'ProjModalInstanceCtrl',
+            size: "lg",
+            resolve: {
+                project: function () {
+                    return project; 
+                }
+            }
+        });
+    };
+});
+
+/*Controller for modal dialog*/
+angular.module('theatreProjApp')
+  .controller('ProjModalInstanceCtrl', function ($scope, $modalInstance, $http, socket, Auth, project) {
+
+    $scope.currProject = project;
+    $scope.editorEnabledName = false;
+
+    $scope.enableEditorName = function() {
+      $scope.editorEnabledName = true;
+      $scope.newName = $scope.currProject.showName;
+    };
+
+    $scope.disableEditor = function() {
+      $scope.editorEnabledName = false;
+    };
+
+    $scope.saveName = function() {
+      $scope.name.$setPristine();
+      $scope.currName = $scope.newName;
+      $scope.currProject.showName = $scope.currName;
+      $http.put('api/projectshows/' + $scope.currProject._id, $scope.currProject);
+      $scope.disableEditor();
+    };
+
+    $scope.close = function () {
+        $modalInstance.close();
+    };
 });
