d81c6164541436d629bab62a85a904ebc19b82a3
Yik Surn Chong <yiksurn.chong@gmail.com>
2015-05-16 13:22:07 +1000

    Solve login redirect error

--- client/app/app.js
+++ client/app/app.js
@@ -43,29 +43,44 @@ angular.module('theatreProjApp', [
     };
   })
 
-  .run(function ($rootScope, $location, Auth, $timeout) {
+  .run(function ($rootScope, Auth, $timeout, $state) {
 
     $rootScope.spinner = false;
+
+    $rootScope.hideSpinner = function () {
+      $rootScope.spinner = false;
+      $timeout.cancel($rootScope.timeoutPromise);
+    }
 
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
       Auth.isLoggedInAsync(function(loggedIn) {
         if (toState.authenticate && !loggedIn) {
-          $location.path('/login');
+          event.preventDefault();
+          $state.go('login');
+        }
+        else {
+          $rootScope.timeoutPromise = $timeout(function () {
+            $rootScope.spinner = true;
+          }, 50);
         }
       });
 
-      $rootScope.timeoutPromise = $timeout(function () {
-        $rootScope.spinner = true;
-      }, 50);
     });
 
     $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
       // Autoscroll to top of page
       document.body.scrollTop = document.documentElement.scrollTop = 0;
 
-      $rootScope.spinner = false;
-      $timeout.cancel($rootScope.timeoutPromise);
+      $rootScope.hideSpinner();
+
+    });
+
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams) {
+      // Autoscroll to top of page
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+      $rootScope.hideSpinner();
 
     });
   });
