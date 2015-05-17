YikSurn/TheatreProj

commit 446c44e3ce03a1aa77723cc47e3b0630ea2a72d2

Bugfix: highlight boxes on hover

client/app/about-us/about-us.scss
@@ -203,7 +203,7 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 	background-repeat: no-repeat;
 	@include transition($animate-duration, 0s);
 
-	&:before {
+	&:before, &:after {
 		content: '';
 		display: block;
 		position: absolute;
@@ -211,6 +211,9 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 		opacity: 1;
 		width: 100%;
 		height: 100%;
+	}
+
+	&:before {
 		background-image: linear-gradient(to bottom right, $panel-dark-shade, transparent 60%);
 		transform: translateZ(-1px);
 	}

commit 57c1ff4ee938b5af12d4979767dbb2f730e5c047

Fixed bug introduced in commit 446c44e3ce03a1aa77723cc47e3b0630ea2a72d2: Hover now uses :before instead of :after (which was getting in the way of clicking the social media icons). Had to flip the top panel for this to show (the highlight was on the inside of the cube before)

client/app/about-us/about-us.controller.js
@@ -230,13 +230,11 @@ angular.module('theatreProjApp')
 			break;
 			
 			case 'top':
-			ret['transform-origin'] = 'top';
-			ret.transform += ' translateZ(-' + len + 'px)'; // move it to align with the back of the cube
-			if (active) {
-				ret.transform += ' rotateX(180deg)'; // unfold it to lay open
-			} else {
-				ret.transform += ' rotateX(90deg)'; // rotate it to lay flat
-				ret.transform += ' translateZ(-' + ($scope.cubeLength - len * 2) + 'px)'; // shift it to align with the post-transformed cube
+			ret['transform-origin'] = 'bottom';
+			ret.transform += ' translateZ(-' + len + 'px)'; // move it back to align with the back of the cube
+			ret.transform += ' translateY(-' + len*2 + 'px)'; // move it up to align with the top edge
+			if (!active) {
+				ret.transform += ' rotateX(-90deg)'; // rotate it to lay flat
 			}
 			break;
 

client/app/about-us/about-us.scss
@@ -99,6 +99,10 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 		background-image: linear-gradient(to top, transparent 40%, $wall-dark-shade 100%);
 	}
 
+	&:hover {
+		cursor: pointer;
+	}
+
 	&.drag-end {
 		@include transition($animate-duration, 0s);
 	}
@@ -187,7 +191,7 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 	@include transform-style(preserve-3d);
 
 	&.inactive:hover {
-		> .carousel-panel:after {
+		> .carousel-panel:before {
 			background: $active-bg;
 		}
 		cursor: pointer;
@@ -203,7 +207,7 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 	background-repeat: no-repeat;
 	@include transition($animate-duration, 0s);
 
-	&:before, &:after {
+	&:before {
 		content: '';
 		display: block;
 		position: absolute;
@@ -211,9 +215,6 @@ $panel-dark-shade: rgba(0,0,0,0.5);
 		opacity: 1;
 		width: 100%;
 		height: 100%;
-	}
-
-	&:before {
 		background-image: linear-gradient(to bottom right, $panel-dark-shade, transparent 60%);
 		transform: translateZ(-1px);
 	}